8872b774adfdd7c8f6923c98f57f938c2ce6b080
caduc <remaier@gmail.com>
2014-10-01 19:56:02 -0400

    copy reader process

--- readingcsv.py
+++ readingcsv.py
@@ -1,3 +1,19 @@
 #
 #  attempts at copying the reading csv
 #
+import csv
+
+#--------------
+'''
+read csv file
+'''
+
+reader = csv.reader(file_obj)
+for row in reader:
+	print(" ".join(row))
+
+#-------
+if __name__ == '__main__':
+	csv_path = "TB_burden_countries_2014-09-27.csv"
+	with open(csv_path, "rb") as f_obj:
+		csv_reader(f_obj)
